29e51ceed89a81e2e7a38482832d7c21a1e2aff7
abhilekhiiitg <abhilekhiiitg@gmail.com>
2020-10-18 20:19:36 +0530

    Update Resource.kt
    Removed space

--- app/src/main/java/com/abhilekh/assignmentapplication/utils/Resource.kt
+++ app/src/main/java/com/abhilekh/assignmentapplication/utils/Resource.kt
@@ -15,7 +15,6 @@ data class Resource<out T>(val status: Status, val data: T?, val message: String
         fun <T> loading(data: T?): Resource<T> {
             return Resource(Status.LOADING, data, null)
         }
-
     }
 
 }
